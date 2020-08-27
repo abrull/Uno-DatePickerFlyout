ab5524c6e21f1afb6bb4700a0e476463b55d3250
Abraham Brull <abrull@chorus.cloud>
2020-08-27 18:31:59 -0400

    Add project files.

---
+++ DatePickerFlyoutTest.Shared/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DatePickerFlyoutTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private DateTimeOffset date;
+
+        public DateTimeOffset Date
+        {
+            get => date;
+            set 
+            { 
+                date = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Date"));
+            }
+        }
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
